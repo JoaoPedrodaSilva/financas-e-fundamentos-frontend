378f26d678704c6444199f11a5493f8dae51870e
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2022-09-27 21:17:05 -0300

    make community page

--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -1,7 +1,27 @@
-import underconstruction from '../underconstruction.webp'
+import { FaInstagram, FaFacebookSquare, FaLinkedin, FaYoutube, FaSpotify } from "react-icons/fa"
+
 
 export const CommunityPage = () => (
     <section className='h-full flex flex-col justify-center items-center gap-4 px-5 lg:px-20'>
-      <img className='w-1/5 self-center pt-20' src={underconstruction} alt="em construção" />
+        <h1 className='text-white text-2xl sm:text-3xl lg:text-4xl self-center text-center mb-6 sm:mb-12'>
+            Junte-se a nossa comunidade!
+        </h1>
+        <div className="flex flex-wrap gap-7 justify-center items-center text-4xl lg:text-5xl text-white">
+            <a className="basis-1/5" href="https://www.instagram.com/financasefundamentos/" target='_blank' rel='noreferrer'>
+                <FaInstagram />
+            </a>
+            <a className="basis-1/5" href="https://www.facebook.com/financasefundamentos" target='_blank' rel='noreferrer'>
+                <FaFacebookSquare />
+            </a>
+            <a className="basis-1/5" href="https://www.linkedin.com/company/financasefundamentos" target='_blank' rel='noreferrer'>
+                <FaLinkedin />
+            </a>
+            <a className="basis-1/5" href="https://www.youtube.com/channel/UCA23qmZxZI2ghbVTQGOvs5w" target='_blank' rel='noreferrer'>
+                <FaYoutube />
+            </a>
+            <a className="basis-1/5" href="https://open.spotify.com/show/1j2CtvGwUbg3Ggfo3ihjk8" target='_blank' rel='noreferrer'>
+                <FaSpotify />
+            </a>
+        </div>
     </section>
-  )
+)
